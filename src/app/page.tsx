6ae678da9ed44eb37a1cf8629238f96ae923ca20
AI Website Generator <generator@yourplatform.com>
2025-10-23 11:31:23 +0000

    Add 0 component(s) to Home:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import Hero from '@/components/sections/home/Hero'
-import Features from '@/components/sections/home/Features'
 
 export default function HomePage() {
   return (
     <>
       <Hero />
     
-      <Features /></>
+</>
   )
 }
